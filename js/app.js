024c5d5f862e27cc47936a13d7f31b2160f9e11e
Tsimur <dopingjr2010@gmail.com>
2025-09-28 14:38:25 -0400

    DudeShape Project commit 8

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-'use strict';
+
+function slidesPlugin(activeSlide = 2) {
+
+    const slides = document.querySelectorAll('.features__blocks-block');
+
+slides[activeSlide].classList.add('active')
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
+
+};
+
+slidesPlugin();
